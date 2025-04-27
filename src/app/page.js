315c9bdc5885dd015e5c621b6475c4348ff903c1
Reed Lathrop <reedlathrop@gmail.com>
2025-04-27 12:14:40 -0500

    added fixed analytics

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 "use client";
 import { useSelectedLayoutSegment } from "next/navigation";
 import { useState, useStatep } from "react";
-import { Analytics } from "@vercel/analytics/react"
 
 function Square({ value, onClick}) {
   return ( 
